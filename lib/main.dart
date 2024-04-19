762ac901a09583c4785c0b6dd1313f8751475460
KStarID <kemal.aziz03@gmail.com>
2024-04-19 21:18:46 +0700

    coba firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,11 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
