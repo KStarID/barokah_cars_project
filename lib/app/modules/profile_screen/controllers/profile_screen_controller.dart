74372b4801e8ff5ff54fbf8c537e996367ffae99
Rizky Ahsan Syarif <ikkiahsan_@MacBook-Pro.local>
2024-06-01 15:52:51 +0700

    astagfirullah

--- lib/app/modules/profile_screen/controllers/profile_screen_controller.dart
+++ lib/app/modules/profile_screen/controllers/profile_screen_controller.dart
@@ -21,87 +21,91 @@ class ProfileScreenController extends GetxController {
     fetchUserData();
   }
 
-  // Modifikasi metode fetchUserData
-Future<void> fetchUserData() async {
-  try {
-    User? user = _auth.currentUser;
-    if (user != null) {
-      DocumentSnapshot userSnapshot =
-          await _userCollection.doc(user.uid).get();
-      if (userSnapshot.exists) {
-        var userData = userSnapshot.data() as Map<String, dynamic>;
-        name.value = userData['name'] ?? '';
-        email.value = userData['email'] ?? '';
-        profileImageUrl.value = userData['profileImageUrl'] ?? '';
+  Future<void> fetchUserData() async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        DocumentSnapshot userSnapshot =
+            await _userCollection.doc(user.uid).get();
+        if (userSnapshot.exists) {
+          var userData = userSnapshot.data() as Map<String, dynamic>;
+          name.value = userData['name'] ?? '';
+          email.value = userData['email'] ?? '';
+          profileImageUrl.value = userData['profileImageUrl'] ?? '';
+        } else {
+          print('No user data found.');
+        }
       } else {
-        print('No user data found.');
+        print('No user logged in.');
       }
-    } else {
-      print('No user logged in.');
+    } catch (e) {
+      print('Error fetching user data: $e');
     }
-  } catch (e) {
-    print('Error fetching user data: $e');
   }
-}
 
-  // Metode untuk mengambil foto dari galeri
   Future<void> pickImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
       File image = File(pickedFile.path);
-      // Panggil metode untuk menyimpan foto ke Firebase
       await uploadImage(image);
     } else {
       print('No image selected.');
     }
   }
 
-  // Modifikasi metode uploadImage
-Future<void> uploadImage(File image) async {
-  try {
-    Get.dialog(
-      const Center(
-        child: SizedBox(
-          height: 100,
-          width: 100,
-          child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFE92027)),
-            strokeWidth: 5,
+  Future<void> uploadImage(File image) async {
+    try {
+      Get.dialog(
+        const Center(
+          child: SizedBox(
+            height: 100,
+            width: 100,
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFE92027)),
+              strokeWidth: 5,
+            ),
           ),
         ),
-      ),
-      barrierDismissible: false,
-    );
+        barrierDismissible: false,
+      );
 
-    // Mendapatkan referensi ke Firebase Storage
-    firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
-        .ref()
-        .child('profile_images')
-        .child('${DateTime.now().millisecondsSinceEpoch}.jpg');
+      firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+          .ref()
+          .child('profile_images')
+          .child('${DateTime.now().millisecondsSinceEpoch}.jpg');
 
-    // Upload foto ke Firebase Storage
-    await ref.putFile(image);
+      await ref.putFile(image);
 
-    // Dapatkan URL foto yang diunggah
-    String imageUrl = await ref.getDownloadURL();
+      String imageUrl = await ref.getDownloadURL();
 
-    // Simpan URL foto ke variabel profileImageUrl
-    profileImageUrl.value = imageUrl;
+      profileImageUrl.value = imageUrl;
 
-    // Simpan URL foto ke Firestore
+      User? user = _auth.currentUser;
+      if (user != null) {
+        await _userCollection.doc(user.uid).update({
+          'profileImageUrl': imageUrl,
+        });
+      }
+
+      Get.back();
+    } catch (e) {
+      print('Error uploading image: $e');
+    }
+  }
+
+  Future<void> updateName(String newName) async {
     User? user = _auth.currentUser;
     if (user != null) {
-      await _userCollection.doc(user.uid).update({
-        'profileImageUrl': imageUrl,
-      });
+      try {
+        await _userCollection.doc(user.uid).update({'name': newName});
+        name.value = newName;
+        Get.snackbar("Sukses", "Nama berhasil diperbarui", backgroundColor: Color(0xFFE92027), colorText: Colors.white);
+      } catch (e) {
+        Get.snackbar("Kesalahan", "Gagal memperbarui nama: $e");
+        Get.snackbar("Kesalahan", "Gagal memperbarui nama: $e", backgroundColor: Color(0xFFE92027), colorText: Colors.white);
+      }
     }
-
-    // Tutup dialog
-    Get.back();
-  } catch (e) {
-    print('Error uploading image: $e');
   }
-}
 }
